9f076c1f49124845816b33a5f830aa3fe5dbbf0d
roger-mont <roger_galvin@hotmail.com>
2023-01-16 14:01:19 -0400

    Projeto Iniciado

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,8 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = (a) => {
+  if (!fetchProductsList) {
+    throw new Error('Termo de Busca não informado') 
+  }
 };
