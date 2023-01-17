29fc219b2b996d5b745ff3041e8919713023e990
roger-mont <roger_galvin@hotmail.com>
2023-01-17 17:40:37 -0400

    requisito 5 concluido

--- src/main.js
+++ src/main.js
@@ -8,12 +8,18 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 const x = async () => {
   const productArea = document.querySelector('.products');
   productArea.innerHTML += '<span class="loading">carregando...</span>';
-  const pc = await fetchProductsList('computador');
-  const loading = document.querySelector('.loading');
-  productArea.removeChild(loading);
-  pc.forEach((e) => {
-    productArea.appendChild(createProductElement(e));
-  });
+  try {
+    const pc = await fetchProductsList('computador');
+    const loading = document.querySelector('.loading');
+    productArea.removeChild(loading);
+    pc.forEach((e) => {
+      productArea.appendChild(createProductElement(e));
+    });
+  } catch {
+    productArea.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+    productArea.classList.add('error');
+    productArea.classList.remove('products');
+  }
 };
 
 window.onload = x();
