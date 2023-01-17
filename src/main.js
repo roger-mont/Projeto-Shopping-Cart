7255dba126e8158b8d085240ff2bcb389370c5a1
roger-mont <roger_galvin@hotmail.com>
2023-01-17 17:15:27 -0400

    requisito 4 completo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const x = async () => {
+  const productArea = document.querySelector('.products');
+  productArea.innerHTML += '<span class="loading">carregando...</span>';
+  const pc = await fetchProductsList('computador');
+  const loading = document.querySelector('.loading');
+  productArea.removeChild(loading);
+  pc.forEach((e) => {
+    productArea.appendChild(createProductElement(e));
+  });
+};
+
+window.onload = x();
