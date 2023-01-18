0a67d492039e2225de918a441d49d48e2d31cf58
roger-mont <roger_galvin@hotmail.com>
2023-01-18 15:02:06 -0400

    requisito 8 completo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
+import { saveCartID } from './helpers/cartFunctions';
 import { searchCep } from './helpers/cepFunctions';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+// Funcao para capturar o ID do produto selecionado
+const pegaId = () => {
+  const addCartButtons = document.querySelectorAll('.product__add');
+  addCartButtons.forEach((buttom) => {
+    buttom.addEventListener('click', async () => {
+      const idProduct = buttom.parentNode.firstChild.innerText;
+      saveCartID(idProduct);
+      const produto = await fetchProduct(idProduct);
+      const cartProduct = createCartProductElement(produto);
+      const cartProducts = document.querySelector('.cart__products');
+      cartProducts.appendChild(cartProduct);
+    });
+  });
+};
 
 const x = async () => {
   const productArea = document.querySelector('.products');
@@ -15,6 +31,7 @@ const x = async () => {
     pc.forEach((e) => {
       productArea.appendChild(createProductElement(e));
     });
+    pegaId();
   } catch {
     productArea.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
     productArea.classList.add('error');
